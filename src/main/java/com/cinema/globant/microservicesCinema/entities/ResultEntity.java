10e89976019f7a6b0ee048f0c12656a8207d0198
edisonM21 <edisontapias2003@gmail.com>
2023-06-30 15:23:58 -0500

    Se realiza CRUD para la entidad de peliculas

--- src/main/java/com/cinema/globant/microservicesCinema/entities/ResultEntity.java
+++ src/main/java/com/cinema/globant/microservicesCinema/entities/ResultEntity.java
@@ -22,9 +22,11 @@ public class ResultEntity {
     public long movie;
     public boolean adult;
     public String backdrop_path;
+    public ArrayList<Integer> genre_ids;
     public int id;
     public String original_language;
     public String original_title;
+    @Column(length = 1000)
     public String overview;
     public double popularity;
     public String poster_path;
@@ -39,10 +41,11 @@ public class ResultEntity {
 
     }
 
-    public ResultEntity(long movie, boolean adult, String backdrop_path, int id, String original_language, String original_title, String overview, double popularity, String poster_path, String release_date, String title, boolean video, double vote_average, int vote_count, boolean nowPlaying) {
+    public ResultEntity(long movie, boolean adult, String backdrop_path, ArrayList<Integer> genre_ids, int id, String original_language, String original_title, String overview, double popularity, String poster_path, String release_date, String title, boolean video, double vote_average, int vote_count, boolean nowPlaying) {
         this.movie = movie;
         this.adult = adult;
         this.backdrop_path = backdrop_path;
+        this.genre_ids = genre_ids;
         this.id = id;
         this.original_language = original_language;
         this.original_title = original_title;
